474169ea39e03f91370c561adb817d32f4a55d84
amalik4 <Adil_Malik@intuit.com>
2018-10-03 10:08:46 -0600

    deploy script comment

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-scp ../web-app-ci-cd-with-travis-ci-amalik2 ubuntu@162.246.157.110:~/web-app-ci-cd-with-travis-ci-amalik2
+#scp ../web-app-ci-cd-with-travis-ci-amalik2 ubuntu@162.246.157.110:~/web-app-ci-cd-with-travis-ci-amalik2
