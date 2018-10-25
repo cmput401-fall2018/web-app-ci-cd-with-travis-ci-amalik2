3dcebb9f71879874ca65ae793d4b84633f706b71
amalik2 <malik2@ualberta.ca>
2018-10-24 23:20:05 -0600

    test errors fixed

--- test_service.py
+++ test_service.py
@@ -5,7 +5,7 @@ from unittest.mock import MagicMock, patch, mock_open
 
 def testDivideWithValues(instance, mockValue, divideArg, expected):
 	instance.bad_random = MagicMock(mockValue)
-	assert instance.divide(actualValue) == expected
+	assert instance.divide(divideArg) == expected
 	instance.bad_random.assert_called_once()
 
 class ServiceTests(unittest.TestCase):
@@ -22,25 +22,25 @@ class ServiceTests(unittest.TestCase):
 		testDivideWithValues(self.instance, 20000, -10000, -2)
 		
 	def test_abs_plus(self):
-		assert instance.abs_plus(-1) == 0
-		assert instance.abs_plus(1) == 2
-		assert instance.abs_plus(0) == 1
-		assert instance.abs_plus(-2) == -1
-		assert instance.abs_plus(-2147483648) == -2147483647
-		assert instance.abs_plus(2147483647) == 2147483648
+		assert self.instance.abs_plus(-1) == 0
+		assert self.instance.abs_plus(1) == 2
+		assert self.instance.abs_plus(0) == 1
+		assert self.instance.abs_plus(-2) == -1
+		assert self.instance.abs_plus(-2147483648) == -2147483647
+		assert self.instance.abs_plus(2147483647) == 2147483648
 
 	def test_complicated_function(self):
 		self.instance.divide = MagicMock(25)
-		self.assertRaises(SyntaxError, self.instance.complicated_function, 25)
+		self.assertRaises(SyntaxError, Service.complicated_function, 25)
 		
 		self.instance.divide = MagicMock(0)
-		self.assertRaises(SyntaxError, self.instance.complicated_function, 9)
+		self.assertRaises(SyntaxError, Service.complicated_function, 9)
 		
 		self.instance.divide = MagicMock(100)
-		self.assertRaises(SyntaxError, self.instance.complicated_function, 2)
+		self.assertRaises(SyntaxError, Service.complicated_function, 2)
 		
 		self.instance.divide = MagicMock(-25)
-		self.assertRaises(SyntaxError, self.instance.complicated_function, 2)
+		self.assertRaises(SyntaxError, Service.complicated_function, 2)
 
 	@patch("builtins.open")
 	@patch("random.randint")
@@ -48,19 +48,19 @@ class ServiceTests(unittest.TestCase):
 		# integers
 		mockOpen.return_value = [1, 4, 7]
 		randintMock.return_value = 2
-		assert self.instance.bad_random() == 2
+		assert Service.bad_random() == 2
 		randintMock.assert_called_once_with_args(0, 2)
 		
 		# empty
 		mockOpen.return_value = []
 		randintMock.return_value = -1
-		assert self.instance.bad_random() == -1
+		assert Service.bad_random() == -1
 		randintMock.assert_called_once_with_args(0, -1)
 		
 		# float
 		mockOpen.return_value = [5.2]
 		randintMock.return_value = 0
-		assert self.instance.bad_random() == 0
+		assert Service.bad_random() == 0
 		randintMock.assert_called_once_with_args(0, 0)
 		
 		# non-numeric values
