1769b5667f0ceedfae22e83567783c0ba3180406
amalik2 <malik2@ualberta.ca>
2018-10-24 23:13:30 -0600

    playing with config

--- selenium_test.py
+++ selenium_test.py
@@ -1,9 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 
 def test_home():
-	driver = webdriver.Chrome()
+	options = Options()
+	options.add_argument("--no-sandbox")
+	options.add_argument("--headless")
+	driver = webdriver.Chrome(chrome_options=options)
 	driver.get("http://162.246.157.110:8000/")
+	
 	assert driver.find_element_by_id("name") != None
 	assert driver.find_element_by_id("about") != None
 	assert driver.find_element_by_id("education") != None
