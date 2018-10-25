608bab80cc2732abdfb99cdf1023fbb896ccbb2e
amalik2 <malik2@ualberta.ca>
2018-10-24 23:38:06 -0600

    attempt tests again

--- test_service.py
+++ test_service.py
@@ -3,24 +3,31 @@ import unittest
 import random
 from unittest.mock import MagicMock, patch, mock_open
 
+class MockFile:
+	def __init__(self, returnValue):
+		self.returnValue = returnValue
+	
+	def readlines(self):
+		return self.returnValue
+
 class ServiceTests(unittest.TestCase):
 	def setUp(self):
 		self.instance = Service()
 
 		
-	def _testDivideWithValues(instance, mockValue, divideArg, expected):
+	def _testDivideWithValues(self, mockValue, divideArg, expected):
 		Service.bad_random = MagicMock(mockValue)
-		assert instance.divide(divideArg) == expected
+		assert self.instance.divide(divideArg) == expected
 		Service.bad_random.assert_called_once()
 		
 	def test_divide(self):
-		self._testDivideWithValues(self.instance, 1, 5, 0.2)
-		self._testDivideWithValues(self.instance, 1, 0, float("inf"))
-		self._testDivideWithValues(self.instance, 0, 5, 0)
-		self._testDivideWithValues(self.instance, 5, 5, 1)
-		self._testDivideWithValues(self.instance, -3, 5, -0.6)
-		self._testDivideWithValues(self.instance, -2, -4, 0.5)
-		self._testDivideWithValues(self.instance, 20000, -10000, -2)
+		self._testDivideWithValues(1, 5, 0.2)
+		self._testDivideWithValues(1, 0, float("inf"))
+		self._testDivideWithValues(0, 5, 0)
+		self._testDivideWithValues(5, 5, 1)
+		self._testDivideWithValues(-3, 5, -0.6)
+		self._testDivideWithValues(-2, -4, 0.5)
+		self._testDivideWithValues(20000, -10000, -2)
 		
 	def test_abs_plus(self):
 		assert self.instance.abs_plus(-1) == 2
@@ -47,26 +54,25 @@ class ServiceTests(unittest.TestCase):
 	@patch("random.randint")
 	def test_bad_random(self, mockOpen, randintMock):
 		# integers
-		mockOpen.return_value = [1, 4, 7]
-		mockOpen.return_value.readlines = [1, 4, 7]
+		mockOpen.return_value = MockFile([1, 4, 7])
 		randintMock.return_value = 2
 		assert Service.bad_random() == 2
 		randintMock.assert_called_once_with_args(0, 2)
 		
 		# empty
-		mockOpen.return_value = []
+		mockOpen.return_value = MockFile([])
 		randintMock.return_value = -1
 		assert Service.bad_random() == -1
 		randintMock.assert_called_once_with_args(0, -1)
 		
 		# float
-		mockOpen.return_value = [5.2]
+		mockOpen.return_value = MockFile([5.2])
 		randintMock.return_value = 0
 		assert Service.bad_random() == 0
 		randintMock.assert_called_once_with_args(0, 0)
 		
 		# non-numeric values
-		mockOpen.return_value = [1, "a", 7]
+		mockOpen.return_value = MockFile([1, "a", 7])
 		self.assertRaises(ValueError, Service.bad_random)
 		
 		# no file found
