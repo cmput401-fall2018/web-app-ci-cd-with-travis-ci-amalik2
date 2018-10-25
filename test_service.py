5abf3eab69fbe20c3320aa6375a62d8481421964
amalik2 <malik2@ualberta.ca>
2018-10-24 23:32:28 -0600

    moved util function inside class

--- test_service.py
+++ test_service.py
@@ -3,23 +3,24 @@ import unittest
 import random
 from unittest.mock import MagicMock, patch, mock_open
 
-def testDivideWithValues(instance, mockValue, divideArg, expected):
-	Service.bad_random = MagicMock(mockValue)
-	assert instance.divide(divideArg) == expected
-	Service.bad_random.assert_called_once()
-
 class ServiceTests(unittest.TestCase):
 	def setUp(self):
 		self.instance = Service()
 
+		
+	def _testDivideWithValues(instance, mockValue, divideArg, expected):
+		Service.bad_random = MagicMock(mockValue)
+		assert instance.divide(divideArg) == expected
+		Service.bad_random.assert_called_once()
+		
 	def test_divide(self):
-		testDivideWithValues(self.instance, 1, 5, 0.2)
-		testDivideWithValues(self.instance, 1, 0, float("inf"))
-		testDivideWithValues(self.instance, 0, 5, 0)
-		testDivideWithValues(self.instance, 5, 5, 1)
-		testDivideWithValues(self.instance, -3, 5, -0.6)
-		testDivideWithValues(self.instance, -2, -4, 0.5)
-		testDivideWithValues(self.instance, 20000, -10000, -2)
+		self._testDivideWithValues(self.instance, 1, 5, 0.2)
+		self._testDivideWithValues(self.instance, 1, 0, float("inf"))
+		self._testDivideWithValues(self.instance, 0, 5, 0)
+		self._testDivideWithValues(self.instance, 5, 5, 1)
+		self._testDivideWithValues(self.instance, -3, 5, -0.6)
+		self._testDivideWithValues(self.instance, -2, -4, 0.5)
+		self._testDivideWithValues(self.instance, 20000, -10000, -2)
 		
 	def test_abs_plus(self):
 		assert self.instance.abs_plus(-1) == 2
@@ -46,7 +47,7 @@ class ServiceTests(unittest.TestCase):
 	@patch("random.randint")
 	def test_bad_random(self, mockOpen, randintMock):
 		# integers
-		mockOpen.return_value = [1, 4, 7]
+		mockOpen.return_value = StringIO("1\n4\n7")
 		randintMock.return_value = 2
 		assert Service.bad_random() == 2
 		randintMock.assert_called_once_with_args(0, 2)
